7ad3a2293f930a1108ed5db06e8c40f30739bce2
H82M19\Student <Student>
2025-03-08 10:42:37 +0700

    feat: Add form for saving new courses and handle form submission

--- lab04_nodejs-mvc/index.js
+++ lab04_nodejs-mvc/index.js
@@ -4,6 +4,7 @@ const PORT = 3000;
 let courses = require("./data");
 // Register middleware
 app.use(express.json({extended: false}));
+app.use(express.urlencoded({ extended: true })); // Dùng để phân tích dữ liệu gửi từ form
 app.use(express.static("./views")); // Cho phép dùng các tài nguyên tĩnh như css, js, images,...
 
 // Configs view engine
@@ -12,6 +13,26 @@ app.set("views", "./views"); // Nội dung render trang web sẽ nằm trong th�
 
 app.get("/", (req, res) => {
     return res.render("index", {courses}); // 
+});
+
+app.post("/save", (req, res) => {
+    const id = Number(req.body.id);
+    console.log(id);
+    const name = req.body.name;
+    const course_type = req.body.course_type;
+    const semester = req.body.semester;
+    const department = req.body.department;
+
+    const params = {
+        "id": id,
+        "name": name,
+        "course_type": course_type,
+        "semester": semester,
+        "department": department
+    }
+    console.log(params);
+    courses.push(params);
+    return res.redirect("/");
 });
 
 app.listen(3000, () => {
