8e6b261a0f1591b4a8a0c6fdd6f0c4981eed58f1
H82M19\Student <Student>
2025-03-08 09:59:44 +0700

    chore: Refactor middleware and view engine configuration

--- lab04_nodejs-mvc/index.js
+++ lab04_nodejs-mvc/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 
+// Register middleware
 app.use(express.json({extended: false}));
-app.use(express.static("./views"));
-app.set("view engine", "ejs");
-app.set("views", "./views");
+app.use(express.static("./views")); // Cho phép dùng các tài nguyên tĩnh như css, js, images,...
+
+// Configs view engine
+app.set("view engine", "ejs"); // Khai báo rằng app sẽ dùng engine ejs để render trang web
+app.set("views", "./views"); // Nội dung render trang web sẽ nằm trong thư mục tên "views"
 
 app.get("/", (req, res) => {
-    return res.render("index");
+    return res.render("index"); // 
 });
 
 app.listen(3000, () => {
