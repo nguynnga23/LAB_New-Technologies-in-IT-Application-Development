9c5ee03d56932b1d9a3423900f1e6d87952e0cc0
H82M19\Student <Student>
2025-03-29 11:12:38 +0700

    feat: implement upload image to S3

---
+++ lab07/src/services/upload.service.js
@@ -0,0 +1,13 @@
+const {s3} = require("../configs/aws.helper");
+const uploadToS3 = (file) => {
+    const params = {
+        Bucket: process.env.BUCKET_NAME,
+        Key: `${Date.now()}_${file.originalName}`,
+        Body: file.buffer,
+        ContentType: file.mimetype
+    };
+
+    return s3.upload(params).promise();
+}
+
+module.exports = uploadToS3;
